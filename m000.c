b0a18ed410501047a00f8dde6b4b6e6ec72c32ac
dccolque <dccolque@usp.br>
2020-07-05 17:47:49 +0000

    Quinta Entrega

--- m000.c
+++ m000.c
@@ -1,19 +1,13 @@
 /* m000.c - Pitagorean name number
-
    This program takes the a person's full name read as a string from 
    from the standard input and output its Pitagorean Number as shown 
    in reference [1] at the bottom of this source file.
-
    E.g.
-
       command line    : John Doe
       expected output : 35
-
    Directions:
-
       Please, edit function roman();
       do no not change function main().
-
  */
 
 #include <stdio.h>
@@ -23,11 +17,33 @@
 
 /* Write the number 'n' in Roman numerals.*/
 
+int np (char c)
+
+{
+  int n;
+  if( (c >= 'a') && (c <= 'z')){
+    n = (c-'a') % 9 + 1;
+  }
+  else 
+   if( (c >= 'A') && (c <= 'Z')){
+   n = (c-'a'+32) % 9 + 1;
+   }
+   else
+   {
+     n=0;
+   }
+  return n;
+}
 int pitagorean (char *s)
 {
-  return 0;
+  int i=0, count=0; 
+  while(s[i] != 0)
+  {
+   count = count + np (s[i]);
+   i++;
+  }
+  return count;
 }
-
 /* Do not edit function main. */
 
 int main (int argc, char **argv)
@@ -46,7 +62,6 @@ int main (int argc, char **argv)
 
 /*  
     References:
-
     [1] Pitagorean name number
     https://en.wikipedia.org/wiki/Numerology#Pythagorean_system
 */
